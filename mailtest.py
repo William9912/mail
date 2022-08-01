106b6f53d739fa65d4e85db190168608a2910e87
William9912 <62931379+William9912@users.noreply.github.com>
2022-08-01 15:19:40 +0800

    Update mailtest.py

--- mailtest.py
+++ mailtest.py
@@ -2,7 +2,7 @@ import yagmail
 
 def sendToMiao():
     username = "2358634245@qq.com"
-    password = 'ofcmwcgbshvjdjce'
+    password = 'zmmofcmwcgbshvjdjcelym'
     host = 'smtp.qq.com'
     yag = yagmail.SMTP(user=username, password=password, host=host)
     yag.send('williamlym9912@163.com','睡觉觉提醒', ['可爱啊苗苗', '放下你的手机哦，不要再看抖音呢', '应该要呼呼睡大觉呢哦'])
